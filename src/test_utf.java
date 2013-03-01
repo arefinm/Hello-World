38e9c4d0fb88fe4dcb42cf2d504bb02cb06bf156
Moshiul Arefin <marefin@globalintertech.com>
2013-02-28 20:36:47 -0600

    commit

---
+++ src/test_utf.java
@@ -0,0 +1,33 @@
+
+public class test_utf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 
+		 * 1. reserve <name> – inputs the name of a person and reserves a seat against this name. If the theater does not
+	have enough seats(completely booked), no seat is assigned and the command responds with message: ‘Sold out -
+	No seat available’. If a reservation has already been made under that name, then the command responds with
+	message: ‘Seat already booked against the name provided’.
+	Otherwise, a seat is reserved against the name provided and the client is relayed a message: ‘Seat assigned to you
+	is <seat-number>’.
+	2. bookSeat <name> <seatNum> – behaves similar to reserve command, but imposes an additional constraint that
+	a seat is reserved if and only if there is no existing reservation against name and the seat having the number seatNum
+	is available. If there is no existing reservation but seatNum is not available, the response is: ‘<seatNum> is not
+	available’.
+	3. search <name> – returns the seat number reserved for name. If no reservation is found for name the system responds
+	with a message: ‘No reservation found for <name>’.
+	4. delete <name> – frees up the seat allocated to that person. The command returns the seat number that was
+	released. If no existing reservation was found, responds with: ‘No reservation found for <name>’.
+	Here <text> is used to indicate the value of the parameter denoted by text in the given context. Note that you
+	also have to write the client program that takes input from the user and then communicates with a server using sockets.
+	Your program should behave correctly in presence of multiple concurrent clients.
+		 * 
+		 * 
+		 */
+	}
+
+}
